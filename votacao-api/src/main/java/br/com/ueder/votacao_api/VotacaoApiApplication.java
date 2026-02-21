a255439f2bb88d294b8db1351de937e9e2b793bc
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-02-21 20:39:01 -0300

    Criação da Api no Java com Spring Boot

---
+++ votacao-api/src/main/java/br/com/ueder/votacao_api/VotacaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ueder.votacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaoApiApplication.class, args);
+	}
+
+}
